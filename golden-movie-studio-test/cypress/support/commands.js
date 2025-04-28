657340b5505faf1b02dd3fe697554127048e0c54
David Teixeira de Masin <davidteixeira.info@gmail.com>
2025-04-28 18:33:47 -0300

    uso de comandos customizados para refatorar projeto

--- golden-movie-studio-test/cypress/support/commands.js
+++ golden-movie-studio-test/cypress/support/commands.js
@@ -1,25 +1,10 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("preencherCadastro", (data) => {
+
+    cy.get("#signup-firstname").type(data.firstName);
+    cy.get("#signup-lastname").type(data.lastName);
+    cy.get("#signup-email").type(data.email);
+    cy.get("#signup-phone").type(data.phone);
+    cy.get("#signup-password").type(data.password);
+    cy.get("#signup-button").click();
+
+});
